acda3ac8f546c8084799ca7d260f1c7781354645
vchho <12765624+vchho@users.noreply.github.com>
2023-11-20 20:37:10 -0800

    updated packages and shadcn components

---
+++ components/date-picker.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Calendar } from "./ui/calendar";
+import { SelectSingleEventHandler } from "react-day-picker";
+
+interface DatePickerProps {
+  value: Date | undefined;
+  setValue: (date: Date | undefined) => void;
+}
+
+export function DatePicker({ setValue, value }: DatePickerProps) {
+  // https://github.com/shadcn-ui/ui/issues/901#issuecomment-1719716399
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [date, setDate] = useState<Date>();
+
+  useEffect(() => {
+    setValue(date);
+  }, [date, setValue]);
+
+  const handleOnSelect: SelectSingleEventHandler = (date) => {
+    // onSelect?.(date)
+    setDate(date);
+    setIsPopoverOpen(false)
+  }
+
+  return (
+    <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "max-w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          today={value || new Date()}
+          selected={value}
+          // onSelect={setDate}
+          onSelect={handleOnSelect}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
---
+++ components/ui/header.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./button";
+
+interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {
+  title: string;
+  description?: string | null;
+  size?: "default" | "sm";
+  goBackLink?: string;
+  trimDesc?: boolean;
+}
+
+export function Header({
+  title,
+  description,
+  size = "default",
+  goBackLink,
+  className,
+  ...props
+}: HeaderProps) {
+  return (
+    <div className={cn("grid gap-1", className)} {...props}>
+      {goBackLink && <ShowBack href={goBackLink} />}
+      <h1
+        className={cn(
+          "line-clamp-1 text-3xl font-bold tracking-tight py-1",
+          size === "default" && "md:text-4xl",
+        )}
+      >
+        {title}
+      </h1>
+      {description ? (
+        <p className="text-muted-foreground text-md">{description}</p>
+      ) : null}
+    </div>
+  );
+}
+
+export const ShowBack = ({ href }: { href: string }) => {
+  return (
+    <Link
+      href={href}
+      className={cn(buttonVariants({ variant: "link" }), "w-fit px-0")}
+    >
+      Go back
+    </Link>
+  );
+};
