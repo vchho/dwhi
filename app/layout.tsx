bb0efc76bf7133074e936ed3781752191ff3097f
vchho <12765624+vchho@users.noreply.github.com>
2023-12-05 16:36:54 -0800

    Added toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ContextProvider } from "@/components/context-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,6 +35,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <ContextProvider>{children}</ContextProvider>
+          <Toaster />
         </body>
       </html>
     </>
