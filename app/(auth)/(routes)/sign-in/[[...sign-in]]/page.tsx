77a60a55d0d6cdfaf157f143815454b24d027084
vchho <12765624+vchho@users.noreply.github.com>
2023-12-07 21:43:08 -0800

    temp sign in page

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,22 @@
-import { SignIn } from "@clerk/nextjs";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
 
 const LoginPage = () => {
   return (
-    <SignIn />
-  )
-}
+    <Button
+      variant="ghost"
+      size="sm"
+      onClick={() =>
+        signIn("discord", {
+          callbackUrl: `${location.origin}/pantry`,
+        })
+      }
+    >
+      Log in
+    </Button>
+  );
+};
 
 export default LoginPage;
