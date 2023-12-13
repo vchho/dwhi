8078beb4f5dbf976983d9262f277e3b4a03925d2
vchho <12765624+vchho@users.noreply.github.com>
2023-12-12 22:27:33 -0800

    Added dropdown shadcn component

---
+++ components/dropdown.tsx
@@ -0,0 +1,122 @@
+import React, { FC, forwardRef, useEffect, useState } from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "./ui/command";
+import { ScrollArea } from "./ui/scroll-area";
+
+// import { ComboBoxItemType } from "@/types/item-type";
+
+interface ComboboxProps extends React.HTMLAttributes<HTMLInputElement> {
+  placeholder: string;
+  data: any[];
+  setState: (genre: string) => void;
+  selectedOption?: string;
+  disabled?: boolean;
+  large?: boolean;
+  reset?: boolean;
+}
+
+export const Combobox: FC<ComboboxProps> = forwardRef<
+  HTMLInputElement,
+  ComboboxProps
+>(
+  (
+    {
+      data,
+      placeholder,
+      setState,
+      selectedOption,
+      disabled,
+      large,
+      reset,
+      ...props
+    },
+    ref,
+  ) => {
+    const [open, setOpen] = useState(false);
+
+    const selectedGenre = data.find((item) => item.label === selectedOption);
+
+    const [value, setValue] = useState(selectedGenre?.value ?? "");
+
+    useEffect(() => {
+      if (!reset) return;
+
+      setValue("");
+    }, [reset]);
+
+    useEffect(() => {
+      if (value) {
+        const findIndex = data.findIndex((item) => item.value === value);
+
+        setState(data[findIndex].value);
+      } else {
+        setState("");
+      }
+    }, [value, setState, data]);
+
+    return (
+      <Popover open={open} onOpenChange={setOpen} {...props}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between"
+          >
+            {value
+              ? data.find((item) => item.value === value)?.label
+              : placeholder}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput
+              placeholder={placeholder}
+              ref={ref}
+              disabled={disabled}
+            />
+            <CommandEmpty>Search not found.</CommandEmpty>
+            <CommandGroup>
+              <ScrollArea
+                className={cn({
+                  "h-72": large,
+                })}
+              >
+                {data.map((item) => (
+                  <CommandItem
+                    key={item.value}
+                    className="cursor-pointer"
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue);
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === item.value ? "opacity-100" : "opacity-0",
+                      )}
+                    />
+                    {item.label}
+                  </CommandItem>
+                ))}
+              </ScrollArea>
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    );
+  },
+);
+Combobox.displayName = "Combobox";
