81423630b9a927688ee5f821639410d0beba4d99
vchho <12765624+vchho@users.noreply.github.com>
2023-12-21 23:18:30 -0800

    added initial alert modal

--- components/pantry-action.tsx
+++ components/pantry-action.tsx
@@ -1,47 +1,145 @@
 "use client";
 
-import React from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { DotsVerticalIcon } from "@radix-ui/react-icons";
+import { DotsHorizontalIcon, DotsVerticalIcon } from "@radix-ui/react-icons";
 
 import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { Switch } from "./ui/switch";
 
 export const PantryAction = ({ pantryId }: { pantryId: string }) => {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          // className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-          className="flex h-8 w-8 p-0"
-        >
-          <DotsVerticalIcon className="h-4 w-4" />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[160px]">
-        <Link
-          href={`/pantry/${pantryId}/edit`}
-          className="hover:cursor-pointer"
-        >
-          <DropdownMenuItem>Edit</DropdownMenuItem>
-        </Link>
-        <DropdownMenuSeparator />
+  const [open, setIsOpen] = useState(false);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
 
-        <Link
-          href={`/pantry/${pantryId}/delete`}
-          className="hover:cursor-pointer"
-        >
-          <DropdownMenuItem>Delete</DropdownMenuItem>
-        </Link>
-      </DropdownMenuContent>
-    </DropdownMenu>
+  return (
+    <>
+      {/* // <DropdownMenu>
+    //   <DropdownMenuTrigger asChild>
+    //     <Button
+    //       variant="ghost"
+    //       // className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+    //       className="flex h-8 w-8 p-0"
+    //     >
+    //       <DotsVerticalIcon className="h-4 w-4" />
+    //       <span className="sr-only">Open menu</span>
+    //     </Button>
+    //   </DropdownMenuTrigger>
+    //   <DropdownMenuContent align="end" className="w-[160px]">
+    //     <Link
+    //       href={`/pantry/${pantryId}/edit`}
+    //       className="hover:cursor-pointer"
+    //     >
+    //       <DropdownMenuItem>Edit</DropdownMenuItem>
+    //     </Link>
+    //     <DropdownMenuSeparator />
+    //     <DropdownMenuGroup>
+    //       <AlertDialog>
+    //         <AlertDialogTrigger>
+    //           <DropdownMenuItem>Profile</DropdownMenuItem>
+    //         </AlertDialogTrigger>
+    //         <AlertDialogContent>
+    //           <AlertDialogHeader>
+    //             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+    //             <AlertDialogDescription>
+    //               This action cannot be undone. This will permanently delete
+    //               your account and remove your data from our servers.
+    //             </AlertDialogDescription>
+    //           </AlertDialogHeader>
+    //           <AlertDialogFooter>
+    //             <AlertDialogCancel>Cancel</AlertDialogCancel>
+    //             <AlertDialogAction>Continue</AlertDialogAction>
+    //           </AlertDialogFooter>
+    //         </AlertDialogContent>
+    //       </AlertDialog>
+    //     </DropdownMenuGroup>
+    //   </DropdownMenuContent>
+    // </DropdownMenu> */}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          {/* <Button variant="secondary">
+            <span className="sr-only">Actions</span>
+            <DotsHorizontalIcon className="h-4 w-4" />
+          </Button> */}
+          <Button
+            variant="ghost"
+            // className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+            className="flex h-8 w-8 p-0"
+          >
+            <DotsVerticalIcon className="h-4 w-4" />
+            <span className="sr-only">Open menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <Link
+            href={`/pantry/${pantryId}/edit`}
+            className="hover:cursor-pointer"
+          >
+            <DropdownMenuItem>
+              <DropdownMenuItem>Edit</DropdownMenuItem>
+            </DropdownMenuItem>
+          </Link>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onSelect={() => setShowDeleteDialog(true)}
+            className="text-red-600"
+          >
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <AlertDialog open={showDeleteDialog} onOpenChange={setShowDeleteDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This pantry will no longer be
+              accessible by you.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <Button
+              variant="destructive"
+              onClick={() => {
+                setShowDeleteDialog(false);
+                // toast({
+                //   description: "This preset has been deleted.",
+                // });
+              }}
+            >
+              Delete
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
