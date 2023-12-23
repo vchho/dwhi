b8b2c2ea43867f0a17715b17fedc168fb2fa7ab1
vchho <12765624+vchho@users.noreply.github.com>
2023-12-22 16:33:09 -0800

    removed dead code and commented code out

--- components/pantry-action.tsx
+++ components/pantry-action.tsx
@@ -5,23 +5,13 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { DotsHorizontalIcon, DotsVerticalIcon } from "@radix-ui/react-icons";
+import { DotsVerticalIcon } from "@radix-ui/react-icons";
 
 import Link from "next/link";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "./ui/dialog";
-import { Label } from "./ui/label";
 
 import {
   AlertDialog,
@@ -31,80 +21,30 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from "./ui/alert-dialog";
-import { Switch } from "./ui/switch";
 
 export const PantryAction = ({ pantryId }: { pantryId: string }) => {
-  const [open, setIsOpen] = useState(false);
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
 
   return (
     <>
-      {/* // <DropdownMenu>
-    //   <DropdownMenuTrigger asChild>
-    //     <Button
-    //       variant="ghost"
-    //       // className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-    //       className="flex h-8 w-8 p-0"
-    //     >
-    //       <DotsVerticalIcon className="h-4 w-4" />
-    //       <span className="sr-only">Open menu</span>
-    //     </Button>
-    //   </DropdownMenuTrigger>
-    //   <DropdownMenuContent align="end" className="w-[160px]">
-    //     <Link
-    //       href={`/pantry/${pantryId}/edit`}
-    //       className="hover:cursor-pointer"
-    //     >
-    //       <DropdownMenuItem>Edit</DropdownMenuItem>
-    //     </Link>
-    //     <DropdownMenuSeparator />
-    //     <DropdownMenuGroup>
-    //       <AlertDialog>
-    //         <AlertDialogTrigger>
-    //           <DropdownMenuItem>Profile</DropdownMenuItem>
-    //         </AlertDialogTrigger>
-    //         <AlertDialogContent>
-    //           <AlertDialogHeader>
-    //             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-    //             <AlertDialogDescription>
-    //               This action cannot be undone. This will permanently delete
-    //               your account and remove your data from our servers.
-    //             </AlertDialogDescription>
-    //           </AlertDialogHeader>
-    //           <AlertDialogFooter>
-    //             <AlertDialogCancel>Cancel</AlertDialogCancel>
-    //             <AlertDialogAction>Continue</AlertDialogAction>
-    //           </AlertDialogFooter>
-    //         </AlertDialogContent>
-    //       </AlertDialog>
-    //     </DropdownMenuGroup>
-    //   </DropdownMenuContent>
-    // </DropdownMenu> */}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          {/* <Button variant="secondary">
-            <span className="sr-only">Actions</span>
-            <DotsHorizontalIcon className="h-4 w-4" />
-          </Button> */}
-          <Button
-            variant="ghost"
-            // className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-            className="flex h-8 w-8 p-0"
-          >
+          <Button variant="ghost" className="flex h-8 w-8 p-0">
             <DotsVerticalIcon className="h-4 w-4" />
             <span className="sr-only">Open menu</span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
+          <Link href={`/pantry/${pantryId}/`} className="hover:cursor-pointer">
+            <DropdownMenuItem>View</DropdownMenuItem>
+          </Link>
+          <DropdownMenuSeparator />
           <Link
             href={`/pantry/${pantryId}/edit`}
             className="hover:cursor-pointer"
           >
-            <DropdownMenuItem>
-              <DropdownMenuItem>Edit</DropdownMenuItem>
-            </DropdownMenuItem>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
           </Link>
           <DropdownMenuSeparator />
           <DropdownMenuItem
