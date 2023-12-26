a7d1bb1a6041bae0ebca7dcee5372bbd71d0eceb
vchho <12765624+vchho@users.noreply.github.com>
2023-12-26 15:52:11 -0800

    fixed global pantry paddings

--- app/(site)/pantry/layout.tsx
+++ app/(site)/pantry/layout.tsx
@@ -7,7 +7,7 @@ interface PantryLayoutProps {
 export default async function PantryLayout({ children }: PantryLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <div className="container flex-1 items-start lg:grid lg:gap-10">
+      <div className="flex-1 items-start lg:grid lg:gap-10 pr-3 pl-3">
         <main className="flex w-full flex-col overflow-hidden">{children}</main>
       </div>
     </div>
