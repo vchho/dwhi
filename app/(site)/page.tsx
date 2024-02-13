a0abb95fccb7f929d21d692835b0e7f90f7e5e68
vchho <12765624+vchho@users.noreply.github.com>
2024-02-13 01:14:04 -0800

    updated text color

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,12 +8,16 @@ export default function Home() {
         aria-labelledby="hero-heading"
         className="mx-auto flex w-full max-w-[64rem] flex-col items-center justify-center gap-4 pb-8 pt-6 text-center md:pb-12 md:pt-10 lg:pt-24 lg:pb-10"
       >
-        <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl lg:leading-[1.1]">
+        <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl ">
           Your all in one pantry management application. Welcome to{" "}
-          <span className="underline">DWHI</span>
+          <span className="underline bg-gradient-to-r from-cyan-400 to-emerald-300 bg-clip-text text-transparent">
+            DWHI
+          </span>
         </h1>
-        DWHI helps you manage what you have stowed away <br />
-        in your fridge or pantry.
+        <span className="text-center text-lg text-muted-foreground">
+          DWHI helps you manage what you have stowed away <br />
+          in your fridge or pantry.
+        </span>
       </section>
     </Shell>
   );
